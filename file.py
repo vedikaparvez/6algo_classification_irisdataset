8dd3a03e70eb513fc9e3f9722536d90b8063e34a
Vedika Parvez <vedikaparvez@hotmail.com>
2020-03-05 00:01:05 +0530

    split data test train

--- file.py
+++ file.py
@@ -41,3 +41,9 @@ pyplot.show()
 # scatter plot matrix
 scatter_matrix(dataset)
 pyplot.show()
+
+# Split-out validation dataset
+array = dataset.values
+X = array[:,0:4]
+y = array[:,4]
+X_train, X_validation, Y_train, Y_validation = train_test_split(X, y, test_size=0.20, random_state=1)
