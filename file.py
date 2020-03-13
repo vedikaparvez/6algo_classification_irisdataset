156d007ba5c3fd10ceccb0377dec1efb650d1afe
Vedika Parvez <vedikaparvez@hotmail.com>
2020-03-13 11:25:55 +0530

    modified code

--- file.py
+++ file.py
@@ -1,3 +1,25 @@
+## STEP - 1
+# Check the versions of libraries 
+# Python version
+import sys
+print('Python: {}'.format(sys.version))
+# scipy
+import scipy
+print('scipy: {}'.format(scipy.__version__))
+# numpy
+import numpy
+print('numpy: {}'.format(numpy.__version__))
+# matplotlib
+import matplotlib
+print('matplotlib: {}'.format(matplotlib.__version__))
+# pandas
+import pandas
+print('pandas: {}'.format(pandas.__version__))
+# scikit-learn
+import sklearn
+print('sklearn: {}'.format(sklearn.__version__))
+
+## STEP - 2
 # import dependencies
 import pandas
 from pandas import read_csv
@@ -18,57 +40,79 @@ from sklearn.svm import SVC
 from sklearn import model_selection
 from sklearn.ensemble import VotingClassifier
 
+## STEP - 3
 # load dataset and check contents
 url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
 names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
 dataset = read_csv(url, names=names)
-# # shape
-# print(dataset.shape)
-# # head
-# print(dataset.head(20))
-# # descriptions
-# print(dataset.describe())
-# # class distribution
-# print(dataset.groupby('class').size())
 
-# visualise the data
+## STEP - 4
+# shape
+print(dataset.shape)
+## STEP - 5
+# head
+print(dataset.head(20))
+## STEP - 6
+# descriptions
+print(dataset.describe())
+## STEP - 7
+# class distribution
+print(dataset.groupby('class').size())
+
+## STEP - 8
 # box and whisker plots
 dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
 pyplot.show()
+## STEP - 9
 # histograms
 dataset.hist()
 pyplot.show()
+## STEP - 10
 # scatter plot matrix
 scatter_matrix(dataset)
 pyplot.show()
 
+## STEP - 11
 # Split-out validation dataset
 array = dataset.values
 X = array[:,0:4]
 y = array[:,4]
 X_train, X_validation, Y_train, Y_validation = train_test_split(X, y, test_size=0.20, random_state=1)
 
-# create the sub models
-estimators = []
-model1 = LogisticRegression(solver='liblinear', multi_class='ovr')
-estimators.append(('logistic', model1))
-model2 = LinearDiscriminantAnalysis()
-estimators.append(('linearda', model2))
-model3 = KNeighborsClassifier()
-estimators.append(('knclassifier', model3))
-model4 = DecisionTreeClassifier()
-estimators.append(('cart', model4))
-model5 = GaussianNB()
-estimators.append(('gaussian', model5))
-model6 = SVC(gamma='auto')
-estimators.append(('svm', model6))
+## STEP - 12
+# Spot Check Algorithms
+models = []
+models.append(('LR', LogisticRegression(solver='liblinear', multi_class='ovr')))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC(gamma='auto')))
 
 # evaluate each model in turn
 results = []
 names = []
-for name, model in estimators:
-    kfold = StratifiedKFold(n_splits=10, random_state=1, shuffle=True)
-    cv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring='accuracy')
-    results.append(cv_results)
-    names.append(name)
-    print('%s: %f (%f)' % (name, cv_results.mean(), cv_results.std()))
+for name, model in models:
+	kfold = StratifiedKFold(n_splits=10, random_state=1)
+	cv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring='accuracy')
+	results.append(cv_results)
+	names.append(name)
+	print('%s: %f (%f)' % (name, cv_results.mean(), cv_results.std()))
+
+## STEP - 13
+# Compare Algorithms
+pyplot.boxplot(results, labels=names)
+pyplot.title('Algorithm Comparison')
+pyplot.show()
+
+## STEP - 14
+# Make predictions on validation dataset
+model = SVC(gamma='auto')
+model.fit(X_train, Y_train)
+predictions = model.predict(X_validation)
+
+## STEP - 15
+# Evaluate predictions
+print(accuracy_score(Y_validation, predictions))
+print(confusion_matrix(Y_validation, predictions))
+print(classification_report(Y_validation, predictions))
